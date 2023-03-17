24adfcd1a6d90d02e87b06e16e802d807be13c88
theowiik <theo.le.wiik@gmail.com>
2023-03-17 20:03:38 +0100

    Generate project

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # Project EpicFootsies 🤫
+
+## Brainstorming
+
+### Core concepts
+
+- Movement
+  - Walk
+  - Jump
+  - Wall jump
+- Shooting
+  - Shoot
+  - Aim
+  - Charge
+- Powerups
+  - Radius
+  - Strobe?
+
+### Work breakdown
+
+#### Paralell
+
+- Movement
+  - \+ Movement powerups
+- Lamps (gun)
+  - \+ Gun powersups
+- Detectors
+  - Placement
+  - Round evaluation
+- Multiplayer
+  - Controller support
+- Graphics
+- Sound
---
+++ Scripts/Player.cs
@@ -0,0 +1,43 @@
+using Godot;
+
+public partial class Player : CharacterBody2D
+{
+    private const float Speed = 300.0f;
+    private const float JumpVelocity = -400.0f;
+
+    // Get the gravity from the project settings to be synced with RigidBody nodes.
+    private float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+
+    public override void _PhysicsProcess(double delta)
+    {
+        Movement(delta);
+    }
+
+    private void Movement(double delta)
+    {
+        Vector2 velocity = Velocity;
+
+        // Add the gravity.
+        if (!IsOnFloor())
+            velocity.Y += gravity * (float)delta;
+
+        // Handle Jump.
+        if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+            velocity.Y = JumpVelocity;
+
+        // Get the input direction and handle the movement/deceleration.
+        // As good practice, you should replace UI actions with custom gameplay actions.
+        Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+        if (direction != Vector2.Zero)
+        {
+            velocity.X = direction.X * Speed;
+        }
+        else
+        {
+            velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+        }
+
+        Velocity = velocity;
+        MoveAndSlide();
+    }
+}
