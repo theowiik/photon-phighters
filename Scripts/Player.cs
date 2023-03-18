1cce0d2995ee9db3c5958e132f595d8fc27ab0ac
Erik Wessman <erikwessman10@gmail.com>
2023-03-18 15:12:30 +0100

    Added initial implementation of movement powerups

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -3,44 +3,55 @@ using Godot;
 public partial class Player : CharacterBody2D
 {
     // General movement
-    [Export]
-    private const float Speed = 500.0f;
-    [Export]
-    private const float FrictionAccelerate = 55.0f;
-    [Export]
-    private const float FrictionDecelerate = 30.0f;
-    
+    private float Speed { get; set; } = 500.0f;
+    private float FrictionAccelerate { get; set; } = 55.0f;
+    private float FrictionDecelerate { get; set; } = 30.0f;
+
     // Jumping
-    [Export]
-    private const float JumpHeight = 100;
-    [Export]
+    private float JumpHeight { get; set; } = 100.0f;
     private const float JumpTimeToPeak = 0.4f;
-    [Export]
     private const float JumpTimeToDescent = 0.35f;
-    private const float JumpVelocity = ((2.0f * JumpHeight) / JumpTimeToPeak) * -1.0f;
-    private const float JumpGravity = ((-2.0f * JumpHeight) / (JumpTimeToPeak * JumpTimeToPeak)) * -1.0f;
-    private const float FallGravity = ((-2.0f * JumpHeight) / (JumpTimeToDescent * JumpTimeToDescent)) * -1.0f;
+    private float JumpVelocity { get; set; }
+    private float JumpGravity { get; set; }
+    private float FallGravity { get; set; }
 
     // Wall sliding
     private bool _isOnWall = false;
     private int _wallDirection = 0;
     private float _timeToWallUnstick = WallUnstickTime;
-    private const float WallJumpSpeed = JumpVelocity;
+    private float WallJumpSpeed;
     private const float WallSlideSpeedMax = 150.0f;
     private const float WallStickTime = 0.25f;
     private const float WallUnstickTime = 0.15f;
 
     // Multiple jumping
-    private int nrPossibleJumps = 1;
+    private int nrPossibleJumps { get; set; } = 1;
     private int _nrCurrentJumps = 0;
 
+    // Weapons
     private Marker2D _gunMarker;
     public Gun Gun { get; private set; }
+
+    // Power up
+    private PowerUpManager powerUpManager = new PowerUpManager();
 
     public override void _Ready()
     {
         _gunMarker = GetNode<Marker2D>("Marker2D");
         Gun = _gunMarker.GetNode<Gun>("Gun");
+        InitializeMovementVariables();
+
+        var btnSpeed = GetNode<Button>("/root/World/Control/SpeedUp");
+        btnSpeed.FocusMode = Control.FocusModeEnum.None;       // Måste sätta denna till None annars kommer knappen tryckas automatiskt när man trycker space
+        btnSpeed.Pressed += () => powerUpManager.IncreaseSpeed(this);
+
+        var btnJumpTimes = GetNode<Button>("/root/World/Control/JumpTimes");
+        btnJumpTimes.FocusMode = Control.FocusModeEnum.None;
+        btnJumpTimes.Pressed += () => powerUpManager.IncreaseMultipleJump(this);
+
+        var btnJumpVelocity = GetNode<Button>("/root/World/Control/JumpVelocity");
+        btnJumpVelocity.FocusMode = Control.FocusModeEnum.None;
+        btnJumpVelocity.Pressed += () => powerUpManager.IncreaseJumpVelocity(this);
     }
 
     public override void _PhysicsProcess(double delta)
@@ -170,12 +181,52 @@ public partial class Player : CharacterBody2D
         return nextVelocity;
     }
 
-    private float GetGravity() {
+    private void InitializeMovementVariables()
+    {
+        this.JumpVelocity = ((2.0f * JumpHeight) / JumpTimeToPeak) * -1.0f;
+        this.JumpGravity = ((-2.0f * JumpHeight) / (JumpTimeToPeak * JumpTimeToPeak)) * -1.0f;
+        this.FallGravity = ((-2.0f * JumpHeight) / (JumpTimeToDescent * JumpTimeToDescent)) * -1.0f;
+        this.WallJumpSpeed = JumpVelocity;
+
+    }
+
+    private float GetGravity()
+    {
         return Velocity.Y < 0.0 ? JumpGravity : FallGravity;
     }
     private void Aim(double delta)
     {
         var direction = GetGlobalMousePosition() - GlobalPosition;
         _gunMarker.Rotation = direction.Angle();
+    }
+
+    public class PowerUpManager
+    {
+        public void IncreaseSpeed(Player player)
+        {
+            player.Speed += 100;
+        }
+
+        public void InstantTurn(Player player)
+        {
+            player.FrictionAccelerate = player.Speed;
+            player.FrictionDecelerate = player.Speed;
+        }
+
+        public void IncreaseMultipleJump(Player player)
+        {
+            player.nrPossibleJumps += 1;
+        }
+
+        public void DecreaseGravity(Player player)
+        {
+            player.JumpGravity -= 100.0f;
+            player.FallGravity -= 100.0f;
+        }
+
+        public void IncreaseJumpVelocity(Player player)
+        {
+            player.JumpVelocity -= 100.0f;
+        }
     }
 }
