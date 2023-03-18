2f92136655314675be8e0adb81995654f7ce86b8
theowiik <42991003+theowiik@users.noreply.github.com>
2023-03-18 17:12:40 +0100

    Pulsate lights

--- Scripts/Light.cs
+++ Scripts/Light.cs
@@ -3,6 +3,7 @@ using Godot;
 public partial class Light : Area2D
 {
     private PointLight2D _pointLight;
+    private AnimationPlayer _animationPlayer;
     public LightMode LightState { get; private set; }
 
     public override void _Ready()
@@ -10,10 +11,13 @@ public partial class Light : Area2D
         _pointLight = GetNode<PointLight2D>("PointLight2D");
         _pointLight.Enabled = false;
         LightState = LightMode.None;
+        _animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
     }
 
     public void SetLight(LightMode lightMode)
     {
+        _animationPlayer.Play("pulsate");
+
         _pointLight.Enabled = true;
         LightState = lightMode == LightMode.Light ? LightMode.Light : LightMode.Dark;
         _pointLight.BlendMode = lightMode == LightMode.Light ? Light2D.BlendModeEnum.Add : Light2D.BlendModeEnum.Sub;
