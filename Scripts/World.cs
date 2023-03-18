d678c3e58c5de4a94ed91c0f6fa2c61b81e76e1d
theowiik <42991003+theowiik@users.noreply.github.com>
2023-03-18 12:15:11 +0100

    Remove score counter

--- Scripts/World.cs
+++ Scripts/World.cs
@@ -3,19 +3,65 @@ using System;
 
 public partial class World : Node2D
 {
-    // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         GetNode<Player>("Player").Gun.ShootDelegate += OnShoot;
     }
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-    {
-    }
-
     private void OnShoot(Node2D bullet)
     {
         AddChild(bullet);
+    }
+
+    public struct Results
+    {
+        public int On;
+        public int Off;
+        public int Neutral;
+    }
+
+    public override void _UnhandledInput(InputEvent @event)
+    {
+        if (@event.IsActionPressed("ui_cancel"))
+        {
+            GetTree().Quit();
+        }
+
+        if (@event.IsActionPressed("ui_down"))
+        {
+            var results = GetResults();
+            GD.Print($"On: {results.On}, Off: {results.Off}, Neutral: {results.Neutral}");
+        }
+    }
+
+    private Results GetResults()
+    {
+        var lights = GetTree().GetNodesInGroup("Light");
+        var results = new Results();
+
+        foreach (var light in lights)
+        {
+            var lightNode = light as Light;
+
+            if (lightNode == null)
+            {
+                throw new Exception("Light node is not a Light!!");
+            }
+
+            switch (lightNode.LightState)
+            {
+                case Light.LightMode.On:
+                    results.On++;
+                    break;
+                case Light.LightMode.Off:
+                    results.Off++;
+                    break;
+                case Light.LightMode.Neutral:
+                    results.Neutral++;
+                    break;
+            }
+        }
+
+        return results;
     }
 }
