6bc7f08a335fdcf13d147579ae2d6ff4d6b15358
theowiik <42991003+theowiik@users.noreply.github.com>
2023-03-18 13:26:12 +0100

    Update colors

--- Scripts/Light.cs
+++ Scripts/Light.cs
@@ -5,7 +5,7 @@ public partial class Light : Node2D
     private PointLight2D _light2D;
     public LightMode LightState { get; private set; } = LightMode.Neutral;
     private Color _whiteColor = new(1, 1, 1);
-    private Color _blackColor = new(0, 0, 0);
+    private Color _blackColor = new(1, 0, 0);
 
     // Used during development
     private bool _isWhite = false;
@@ -21,7 +21,7 @@ public partial class Light : Node2D
         if (Input.IsActionJustPressed("ui_accept"))
         {
             _light2D.Enabled = true;
-            var newColor = _isWhite ? _blackColor : _whiteColor;
+            var newColor = _isWhite ? _whiteColor : _blackColor;
             LightState = _isWhite ? LightMode.On : LightMode.Off;
             _light2D.Color = newColor;
             _isWhite = !_isWhite;
