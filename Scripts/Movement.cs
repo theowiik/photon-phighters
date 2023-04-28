0981d968b5d9a16da52e4da9087fbbb48ddf6725
theowiik <42991003+theowiik@users.noreply.github.com>
2023-04-28 22:32:11 +0200

    Rename fields in Movement

--- Scripts/Movement.cs
+++ Scripts/Movement.cs
@@ -2,55 +2,54 @@ using Godot;
 
 public partial class Movement : Node
 {
-    [Export] public float Speed = 200;
-    [Export] public float JumpForce = 300;
-    [Export] public float DoubleJumpForce = 250;
-    [Export] public float WallJumpForce = 250;
-    [Export] public float GlideGravityScale = 0.5f;
-
     public CharacterBody2D CharacterBody { get; set; }
 
-    private bool _onWall = false;
-    private bool _onFloor = false;
-    private bool _hasDoubleJumped = false;
+    private float _speed = 400;
+    private float _jumpForce = 600;
+    private float _doubleJumpForce = 250;
+    private float _wallJumpForce = 250;
+    private float _glideGravityScale = 0.5f;
+    private float _gravity = 980f;
 
-    private Vector2 _velocity = new Vector2();
+    private Vector2 _velocity = new();
+    private bool _onWall;
+    private bool _onFloor;
+    private bool _hasDoubleJumped;
 
     public override void _Ready()
     {
-        GD.Print("Ready");
     }
 
     public override void _PhysicsProcess(double delta)
     {
-        GD.Print("Physics process");
         var inputDirection = new Vector2(Input.GetActionStrength("p1_right") - Input.GetActionStrength("p1_left"), 0);
 
         // Walking
-        _velocity.X = inputDirection.X * Speed;
+        _velocity.X = inputDirection.X * _speed;
 
         // Jumping
         _onFloor = CharacterBody.IsOnFloor();
         if (_onFloor)
         {
             _hasDoubleJumped = false;
+            _velocity.Y = 0;
         }
 
         if (Input.IsActionJustPressed("p1_jump"))
         {
             if (_onFloor)
             {
-                _velocity.Y = -JumpForce;
+                _velocity.Y = -_jumpForce;
             }
             else if (!_hasDoubleJumped)
             {
-                _velocity.Y = -DoubleJumpForce;
+                _velocity.Y = -_doubleJumpForce;
                 _hasDoubleJumped = true;
             }
             else if (_onWall)
             {
-                _velocity.Y = -WallJumpForce;
-                _velocity.X = -Mathf.Sign(_velocity.X) * WallJumpForce * 0.75f;
+                _velocity.Y = -_wallJumpForce;
+                _velocity.X = -Mathf.Sign(_velocity.X) * _wallJumpForce * 0.75f;
             }
         }
 
@@ -58,11 +57,11 @@ public partial class Movement : Node
         _onWall = CharacterBody.IsOnWall() && !_onFloor && inputDirection.X != 0;
         if (_onWall)
         {
-            CharacterBody.GravityScale = GlideGravityScale;
+            _velocity.Y += _gravity * _glideGravityScale * (float)delta;
         }
         else
         {
-            CharacterBody.GravityScale = 1;
+            _velocity.Y += _gravity * (float)delta;
         }
 
         // Apply movement
