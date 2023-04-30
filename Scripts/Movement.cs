6775f340872a9e31df0b37fe31cf88da68142e0a
elias-carlson <carlson.elias@gmail.com>
2023-04-30 15:57:27 +0200

    Fall with y velocity instead of gravity scale

--- Scripts/Movement.cs
+++ Scripts/Movement.cs
@@ -2,18 +2,17 @@ using Godot;
 
 public partial class Movement : Node
 {
-    [Export] public float Speed = 200;
-    [Export] public float JumpForce = 300;
-    [Export] public float DoubleJumpForce = 250;
-    [Export] public float WallJumpForce = 250;
-    [Export] public float GlideGravityScale = 0.5f;
-
     public CharacterBody2D CharacterBody { get; set; }
+    private const float Gravity = 800;
 
+    private float _speed = 500;
+    private float _jumpForce = 600;
+    private float _doubleJumpForce = 250;
+    private float _wallJumpForce = 250;
+    private float _glideGravityScale = 0.5f;
     private bool _onWall = false;
     private bool _onFloor = false;
     private bool _hasDoubleJumped = false;
-
     private Vector2 _velocity = new Vector2();
 
     public override void _Ready()
@@ -27,30 +26,34 @@ public partial class Movement : Node
         var inputDirection = new Vector2(Input.GetActionStrength("p1_right") - Input.GetActionStrength("p1_left"), 0);
 
         // Walking
-        _velocity.X = inputDirection.X * Speed;
+        _velocity.X = inputDirection.X * _speed;
 
         // Jumping
         _onFloor = CharacterBody.IsOnFloor();
         if (_onFloor)
         {
             _hasDoubleJumped = false;
+            _velocity.Y = 0;
         }
+
+        // Gravity
+        _velocity.Y += Gravity * (float)delta;
 
         if (Input.IsActionJustPressed("p1_jump"))
         {
             if (_onFloor)
             {
-                _velocity.Y = -JumpForce;
+                _velocity.Y = -_jumpForce;
             }
             else if (!_hasDoubleJumped)
             {
-                _velocity.Y = -DoubleJumpForce;
+                _velocity.Y = -_doubleJumpForce;
                 _hasDoubleJumped = true;
             }
             else if (_onWall)
             {
-                _velocity.Y = -WallJumpForce;
-                _velocity.X = -Mathf.Sign(_velocity.X) * WallJumpForce * 0.75f;
+                _velocity.Y = -_wallJumpForce;
+                _velocity.X = -Mathf.Sign(_velocity.X) * _wallJumpForce * 0.75f;
             }
         }
 
@@ -58,15 +61,12 @@ public partial class Movement : Node
         _onWall = CharacterBody.IsOnWall() && !_onFloor && inputDirection.X != 0;
         if (_onWall)
         {
-            CharacterBody.GravityScale = GlideGravityScale;
-        }
-        else
-        {
-            CharacterBody.GravityScale = 1;
+            _velocity.Y = Mathf.Max(_velocity.Y, 0); // prevent wall stickiness
         }
 
         // Apply movement
         CharacterBody.Velocity = _velocity;
         CharacterBody.MoveAndSlide();
     }
+
 }
