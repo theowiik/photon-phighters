32303588df5a15c8010c1ff73245a9bce0ef5951
theowiik <theo.le.wiik@gmail.com>
2023-05-08 11:54:47 +0200

    Add a simple capture point

--- Scripts/CapturePoint.cs
+++ Scripts/CapturePoint.cs
@@ -1,3 +1,4 @@
+using System;
 using Godot;
 using PhotonPhighters.Scripts.Utils;
 
@@ -8,13 +9,67 @@ public partial class CapturePoint : Node2D
     [GetNode("Area2D")]
     private Area2D _area;
 
-    // Called when the node enters the scene tree for the first time.
+    [GetNode("Label")]
+    private Label _label;
+
+    private const float TimeToCapture = 5f;
+
+    /// <summary>
+    ///     Light should reach +TimeToCapture.
+    ///     Dark should reach -TimeToCapture.
+    /// </summary>
+    private float _captureTime;
+
     public override void _Ready()
     {
+        this.AutoWire();
     }
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(double delta)
     {
+        _captureTime += (float)delta;
+        UpdateProgress();
+    }
+
+    private void UpdateProgress()
+    {
+        var progress = CreateProgressBar(-TimeToCapture, TimeToCapture, _captureTime, 10);
+        _label.Text = progress;
+    }
+
+    private static string CreateProgressBar(float minValue, float maxValue, float currentValue, int barLength)
+    {
+        const char first = '-';
+        const char last = '_';
+        
+        if (minValue > maxValue)
+        {
+            throw new ArgumentException("Min value should not be greater than max value.");
+        }
+        
+        if (barLength <= 0)
+        {
+            throw new ArgumentException("Bar length should be greater than 0.");
+        }
+
+        if (currentValue >= maxValue)
+        {
+            return "Light captured";
+            return new string(first, barLength);
+        }
+
+        if (currentValue < minValue)
+        {
+            return "Dark captured";
+            return new string(last, barLength);
+        }
+
+        var progress = (currentValue - minValue) / (maxValue - minValue);
+        var filledLength = (int)(barLength * progress);
+
+        var filled = new string(first, filledLength);
+        var empty = new string(last, barLength - filledLength);
+
+        return filled + empty;
     }
 }
