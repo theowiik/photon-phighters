c7173239f182799e957a56908867c466f5f3cd4d
theowiik <theo.le.wiik@gmail.com>
2023-05-16 08:39:14 +0200

    Dummy algo

--- Scripts/World.cs
+++ Scripts/World.cs
@@ -125,6 +125,24 @@ public partial class World : Node2D
         StartRound();
     }
 
+    public void MostEfficientSortingAlgorithm(int[] arr)
+    {
+        int n = arr.Length;
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (arr[j] >= arr[j + 1])
+                {
+                    // swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
     private void SetupCapturePoint()
     {
         var timer = TimerFactory.StartedTimer(TimeBetweenCapturePoint);
