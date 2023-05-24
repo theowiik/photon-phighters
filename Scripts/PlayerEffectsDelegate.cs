f66ab97844799d8bb700f7abb38a4fc6673b52d0
theowiik <theo.le.wiik@gmail.com>
2023-05-24 11:23:20 +0200

    Whitespaces

--- Scripts/PlayerEffectsDelegate.cs
+++ Scripts/PlayerEffectsDelegate.cs
@@ -6,19 +6,12 @@ namespace PhotonPhighters.Scripts;
 public partial class PlayerEffectsDelegate : Node2D
 {
   public delegate void PlayerEffectPerformed(Node2D effect);
-
   private const string JumpAnimation = "stretch_jump";
-
   private const string LandAnimation = "squish_land";
-
   private const string RunLeftAnimation = "running_left";
-
   private const string RunRightAnimation = "running_right";
-
   private const string SpawnAnimation = "spawn";
-
   private const string Wall = "squish_wall";
-
   private readonly Color _hurtColor = new(0.8f, 0, 0);
 
   private readonly PackedScene _hurtParticlesScene = ResourceLoader.Load<PackedScene>(
