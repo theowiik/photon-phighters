63da354c8d02f7efb352db8fd24d555837a16eeb
theowiik <42991003+theowiik@users.noreply.github.com>
2023-06-01 13:02:40 +0200

    Pitch music based on how close to a win

---
+++ Scripts/MusicPlayer.cs
@@ -0,0 +1,13 @@
+using Godot;
+
+public partial class MusicPlayer : AudioStreamPlayer
+{
+  private const float MinPitch = 1f;
+  private const float MaxPitch = 1.4f;
+
+  public void SetPitch(int lightWins, int darkWins, int scoreToWin)
+  {
+    var greatest = Mathf.Max(lightWins, darkWins);
+    PitchScale = Mathf.Lerp(MinPitch, MaxPitch, (float)greatest / scoreToWin);
+  }
+}
