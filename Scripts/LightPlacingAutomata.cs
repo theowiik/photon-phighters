bee17111be96d0bce6b3dc079b7f74db4ba30941
theowiik <42991003+theowiik@users.noreply.github.com>
2023-06-09 22:08:49 +0200

    New ray pos

--- Scripts/LightPlacingAutomata.cs
+++ Scripts/LightPlacingAutomata.cs
@@ -1,10 +1,10 @@
+using System.Collections.Generic;
 using Godot;
 using PhotonPhighters.Scripts.Utils;
 
 public partial class LightPlacingAutomata : Node2D
 {
-  [GetNode("RayCast2D")]
-  private RayCast2D _rayCast2D;
+  private readonly IList<RayCast2D> _rayCasts = new List<RayCast2D>();
 
   [Signal]
   public delegate void PossibleLightPositionFoundEventHandler(Vector2 globalPosition);
@@ -12,21 +12,40 @@ public partial class LightPlacingAutomata : Node2D
   public override void _Ready()
   {
     this.AutoWire();
+    PlaceRaysInCircle();
+  }
+
+  private void PlaceRaysInCircle()
+  {
+    const int NRays = 5;
+    const int Length = 300;
+
+    for (var i = 0; i < NRays; i++)
+    {
+      var angle = i * 2 * Mathf.Pi / NRays;
+      var ray = new RayCast2D();
+      ray.TargetPosition = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)).Normalized() * Length;
+      AddChild(ray);
+      _rayCasts.Add(ray);
+    }
   }
 
   public override void _Process(double delta)
   {
     // GlobalPosition = GetGlobalMousePosition();
 
-    var hit = _rayCast2D.GetCollider();
-    GD.Print(hit);
-
-    if (hit != null && hit is Node2D node2D && node2D.IsInGroup("floors"))
+    foreach (var ray in _rayCasts)
     {
-      var intersection = _rayCast2D.GetCollisionPoint();
-      GD.Print(intersection);
+      var hit = ray.GetCollider();
+      GD.Print(hit);
 
-      EmitSignal(SignalName.PossibleLightPositionFound, intersection);
+      if (hit is Node2D node2D && node2D.IsInGroup("floors"))
+      {
+        var intersection = ray.GetCollisionPoint();
+        GD.Print(intersection);
+
+        EmitSignal(SignalName.PossibleLightPositionFound, intersection);
+      }
     }
   }
 }
