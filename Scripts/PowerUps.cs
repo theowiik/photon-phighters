411bf685bdea2005060cb9129cf3ac591620afa9
Erik Wessman <erikwessman10@gmail.com>
2023-06-10 16:44:10 +0200

    Moved new powerups to powerups

--- Scripts/PowerUps.cs
+++ Scripts/PowerUps.cs
@@ -232,4 +232,285 @@ public static class PowerUps
       playerWhoSelected.Gun.BulletGravity *= 2; // Bullets drop faster
     }
   }
+
+  public class WallSpider : IPowerUp
+  {
+    // Walljumping briefly increases movement speed
+    public string Name => "Wall Spider";
+    public Rarity Rarity => Rarity.Common;
+    private ulong MsecSinceLastWallJump = 0;
+
+    public void Apply(Player player, Player otherPlayer)
+    {
+      player.PlayerMovementDelegate.PlayerMove += GiveSpeedBoost;
+      player.PlayerMovementDelegate.PlayerWallJump += RecordTimeSinceWallJump;
+    }
+
+    public void GiveSpeedBoost(MovementEvents.PlayerMovementEvent playerMovementEvent)
+    {
+      ulong CurrentTimeMsec = Time.GetTicksMsec();
+      if (CurrentTimeMsec - MsecSinceLastWallJump < 3000)
+      {
+        playerMovementEvent.Speed *= 1.5f;
+      }
+    }
+
+    public void RecordTimeSinceWallJump(MovementEvents.PlayerMovementEvent playerMovementEvent)
+    {
+      MsecSinceLastWallJump = Time.GetTicksMsec();
+    }
+  }
+
+  public class OingoBoingoCurse : IPowerUp
+  {
+    // Opponent is always bouncing
+    public string Name => "Oingo Boingo Curse";
+    public Rarity Rarity => Rarity.Common;
+
+    public void Apply(Player player, Player otherPlayer)
+    {
+      otherPlayer.PlayerMovementDelegate.PlayerLand += ApplyBounce;
+    }
+
+    public void ApplyBounce(MovementEvents.PlayerMovementEvent playerMovementEvent)
+    {
+      playerMovementEvent.Velocity.Y -= 1000;
+    }
+  }
+
+  public class PostLegDayCurse : IPowerUp
+  {
+    // Opponent has to briefly rest between jumps
+    public string Name => "Post Leg Day Curse";
+    public Rarity Rarity => Rarity.Rare;
+    private ulong MsecSinceLastJump = 0;
+
+    public void Apply(Player player, Player otherPlayer)
+    {
+      otherPlayer.PlayerMovementDelegate.PlayerJump += DelayJump;
+    }
+
+    public void DelayJump(MovementEvents.PlayerMovementEvent playerMovementEvent)
+    {
+      ulong CurrentTimeMsec = Time.GetTicksMsec();
+      if (CurrentTimeMsec - MsecSinceLastJump < 2000)
+      {
+        playerMovementEvent.CanJump = false;
+      }
+      else
+      {
+        MsecSinceLastJump = CurrentTimeMsec;
+      }
+    }
+  }
+
+  public class Chronostasis : IPowerUp
+  {
+    // Photons briefly freeze the opponent
+    public string Name => "Chronostasis";
+    public Rarity Rarity => Rarity.Legendary;
+    public ulong MsecSinceLastFreeze = 0;
+
+    public void Apply(Player player, Player otherPlayer)
+    {
+      otherPlayer.PlayerHurt += RecordTimeSinceFreeze;
+      otherPlayer.PlayerMovementDelegate.PlayerMove += FreezePlayer;
+    }
+
+    public void FreezePlayer(MovementEvents.PlayerMovementEvent movementEvent)
+    {
+      ulong CurrentTimeMsec = Time.GetTicksMsec();
+      if (CurrentTimeMsec - MsecSinceLastFreeze < 5000)
+      {
+        movementEvent.CanMove = false;
+        movementEvent.CanJump = false;
+      }
+    }
+
+    public void RecordTimeSinceFreeze(Player player, int damage, PlayerEvents.PlayerHurtEvent playerHurtEvent)
+    {
+      MsecSinceLastFreeze = Time.GetTicksMsec();
+    }
+  }
+
+  /* public class RayleighScattering : IPowerUp
+  {
+    // Photons scatter when hitting surfaces
+    public string Name => "Rayleigh Scattering";
+    public Rarity Rarity => Rarity.Rare;
+
+    public void Apply(Player player, Player otherPlayer)
+    {
+      player.Gun.BulletCollideFloor += ScatterPhotons;
+    }
+
+    public void ScatterPhotons(BulletEvents.BulletEvent bulletCollideFloorEvent)
+    {
+      // TODO: Spawn bullets
+    }
+  } */
+
+  public class BrownianMotionCurse : IPowerUp
+  {
+    // Opponent's photons move erratically
+    public string Name => "Brownian Motion Curse";
+    public Rarity Rarity => Rarity.Rare;
+    private ulong MsecSinceRandomization = 0;
+    private Random rnd = new Random();
+
+    public void Apply(Player player, Player otherPlayer)
+    {
+      player.Gun.BulletFlying += RandomizeDirection;
+    }
+
+    public void RandomizeDirection(BulletEvents.BulletEvent bulletFlyingEvent)
+    {
+      ulong CurrentTimeMsec = Time.GetTicksMsec();
+      if (CurrentTimeMsec - MsecSinceRandomization > 100)
+      {
+        bulletFlyingEvent.Velocity.X += rnd.Next(-200, 200);
+        bulletFlyingEvent.Velocity.Y += rnd.Next(-200, 200);
+      }
+      else
+      {
+        MsecSinceRandomization = CurrentTimeMsec;
+      }
+    }
+  }
+
+  public class FluorescentBurst : IPowerUp
+  {
+    // Getting hurt briefly increases movement speed
+    public string Name => "Fluorescent Burst";
+    public Rarity Rarity => Rarity.Rare;
+
+    public ulong MsecSinceLastHurt = 0;
+
+    public void Apply(Player player, Player otherPlayer)
+    {
+      player.PlayerMovementDelegate.PlayerMove += GiveSpeedBoost;
+      player.PlayerHurt += RecordTimeSinceHurt;
+    }
+
+    public void GiveSpeedBoost(MovementEvents.PlayerMovementEvent playerMovementEvent)
+    {
+      ulong CurrentTimeMsec = Time.GetTicksMsec();
+      if (CurrentTimeMsec - MsecSinceLastHurt < 1000)
+      {
+        playerMovementEvent.Speed *= 1.5f;
+      }
+    }
+
+    public void RecordTimeSinceHurt(Player player, int damage, PlayerEvents.PlayerHurtEvent playerHurtEvent)
+    {
+      MsecSinceLastHurt = Time.GetTicksMsec();
+    }
+  }
+
+  public class SimpleTrigonometry : IPowerUp
+  {
+    // Photons move toward the other player
+    public string Name => "Simple Trigonometry";
+    public Rarity Rarity => Rarity.Rare;
+
+    public Player otherPlayer;
+
+    public void Apply(Player player, Player otherPlayer)
+    {
+      this.otherPlayer = otherPlayer;
+      player.Gun.BulletFlying += MoveToOtherPlayer;
+    }
+
+    public void MoveToOtherPlayer(BulletEvents.BulletEvent bulletEvent)
+    {
+      var vector = this.otherPlayer.Position - bulletEvent.Area2D.Position;
+      var magnitude = vector.Length();
+      bulletEvent.Velocity.X += vector.X / (magnitude / 20);
+      bulletEvent.Velocity.Y += vector.Y / (magnitude / 20);
+    }
+  }
+
+  public class LuminogravitonFluxCurse : IPowerUp
+  {
+    // The opponent's gravity is reversed
+    public string Name => "Luminograviton Flux Curse";
+    public Rarity Rarity => Rarity.Legendary;
+
+    public void Apply(Player player, Player otherPlayer)
+    {
+      otherPlayer.PlayerMovementDelegate.PlayerMove += ReverseGravity;
+    }
+
+    public void ReverseGravity(MovementEvents.PlayerMovementEvent movementEvent)
+    {
+      movementEvent.Gravity *= -1;
+      movementEvent.JumpForce *= -1;
+    }
+  }
+
+  public class PhotonReversifierCurse : IPowerUp
+  {
+    // The opponent's movement is reversed
+    public string Name => "Photon Reversifier Curse";
+    public Rarity Rarity => Rarity.Rare;
+
+    public void Apply(Player player, Player otherPlayer)
+    {
+      otherPlayer.PlayerMovementDelegate.PlayerMove += ReverseMovement;
+    }
+
+    public void ReverseMovement(MovementEvents.PlayerMovementEvent movementEvent)
+    {
+      movementEvent.InputDirection.X *= -1;
+    }
+  }
+
+  public class PheedingPhrenzy : IPowerUp
+  {
+    // Hurting the opponent grows your photons
+    public string Name => "Pheeding Phrenzy";
+    public Rarity Rarity => Rarity.Legendary;
+    public int PhotonDamage = 0;
+    public float PhotonSize = 0;
+
+    public void Apply(Player player, Player otherPlayer)
+    {
+      otherPlayer.PlayerHurt += IncreasePhotonSize;
+      player.Gun.GunShoot += ApplyPhotonSize;
+    }
+
+    public void ApplyPhotonSize(GunEvents.ShootEvent shootEvent)
+    {
+      shootEvent.BulletDamage += PhotonDamage;
+      shootEvent.BulletSizeFactor += PhotonSize;
+    }
+
+    public void IncreasePhotonSize(Player player, int damage, PlayerEvents.PlayerHurtEvent playerHurtEvent)
+    {
+      PhotonDamage += 1;
+      PhotonSize += 0.5f;
+    }
+  }
+
+  public class Randomizer5000 : IPowerUp
+  {
+    // Photons are randomized
+    public string Name => "Randomizer 5000";
+    public Rarity Rarity => Rarity.Common;
+    private Random rnd = new Random();
+
+    public void Apply(Player player, Player otherPlayer)
+    {
+      player.Gun.GunShoot += ApplyRandomization;
+    }
+
+    public void ApplyRandomization(GunEvents.ShootEvent shootEvent)
+    {
+      shootEvent.BulletDamage += rnd.Next(-2, 2);
+      shootEvent.BulletGravity += (float)rnd.NextDouble();
+      shootEvent.BulletSizeFactor += (float)rnd.NextDouble() * rnd.Next(-1, 2);
+      shootEvent.BulletSpeed += rnd.Next(-100, 100);
+      shootEvent.BulletSpread += (float)rnd.NextDouble();
+    }
+  }
 }
