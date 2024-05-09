605a0da0575fe7c745f26a6766415307148994c3
theowiik <42991003+theowiik@users.noreply.github.com>
2024-05-09 20:33:12 +0200

    Dummy commit

--- game/Scripts/Exceptions/NodeNotFoundException.cs
+++ game/Scripts/Exceptions/NodeNotFoundException.cs
@@ -5,5 +5,5 @@ namespace PhotonPhighters.Scripts.Exceptions;
 public class NodeNotFoundException : Exception
 {
   public NodeNotFoundException(string message)
-    : base(message) { }
+    : base(message)  { }
 }
