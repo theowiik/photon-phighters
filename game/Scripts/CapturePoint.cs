ad833a5c906f3913c7b98773e0e47c46794f213b
Elias Kjellberg Carlson <eliaskjellbergcarlson@gmail.com>
2024-06-10 17:41:38 +0200

    Round capture points WIP

--- game/Scripts/CapturePoint.cs
+++ game/Scripts/CapturePoint.cs
@@ -17,8 +17,8 @@ public partial class CapturePoint : Node2D
 
   public enum CapturePointReward
   {
-    Explosion = 9,
-    Kill = 1
+	Explosion = 9,
+	Kill = 1
   }
 
   private const float TimeToCapture = 6f;
@@ -34,8 +34,8 @@ public partial class CapturePoint : Node2D
   [GetNode("ChargePlayer")]
   private AudioStreamPlayer2D _chargePlayer;
 
-  [GetNode("ProgressBar")]
-  private ProgressBar _progressBar;
+  [GetNode("TextureProgressBar")]
+  private TextureProgressBar _textureProgressBar;
 
   private float _radius;
 
@@ -48,146 +48,146 @@ public partial class CapturePoint : Node2D
 
   private bool ChargePlayerPlaying
   {
-    get => _chargePlayer.Playing;
-    set
-    {
-      if (ChargePlayerPlaying == value)
-      {
-        return;
-      }
+	get => _chargePlayer.Playing;
+	set
+	{
+	  if (ChargePlayerPlaying == value)
+	  {
+		return;
+	  }
 
-      if (value)
-      {
-        _chargePlayer.Play();
-      }
-      else
-      {
-        _chargePlayer.Stop();
-      }
-    }
+	  if (value)
+	  {
+		_chargePlayer.Play();
+	  }
+	  else
+	  {
+		_chargePlayer.Stop();
+	  }
+	}
   }
 
   public override void _Draw()
   {
-    var noneColor = new Color(0, 1, 0.2f, 0.3f);
-    var lightColor = new Color(1, 1, 1, 0.3f);
-    var darkColor = new Color(0, 0, 0, 0.3f);
-    var tiedColor = new Color(1, 0f, 0, 0.2f);
+	var noneColor = new Color(0, 1, 0.2f, 0.3f);
+	var lightColor = new Color(1, 1, 1, 0.3f);
+	var darkColor = new Color(0, 0, 0, 0.3f);
+	var tiedColor = new Color(1, 0f, 0, 0.2f);
 
-    var diffPlayers = CalcActiveCaptureDiff();
+	var diffPlayers = CalcActiveCaptureDiff();
 
-    Color color;
-    if (!_playersInside.Any())
-    {
-      color = noneColor;
-    }
-    else
-    {
-      color = diffPlayers switch
-      {
-        0 => tiedColor,
-        > 0 => lightColor,
-        _ => darkColor
-      };
-    }
+	Color color;
+	if (!_playersInside.Any())
+	{
+	  color = noneColor;
+	}
+	else
+	{
+	  color = diffPlayers switch
+	  {
+		0 => tiedColor,
+		> 0 => lightColor,
+		_ => darkColor
+	  };
+	}
 
-    DrawCircle(Vector2.Zero, _radius, color);
+	DrawCircle(Vector2.Zero, _radius, color);
   }
 
   private static CapturePointReward GetRandomReward()
   {
-    // TODO: Use better odds thingy
-    var rewards = new List<CapturePointReward>();
+	// TODO: Use better odds thingy
+	var rewards = new List<CapturePointReward>();
 
-    foreach (CapturePointReward reward in Enum.GetValues(typeof(CapturePointReward)))
-    {
-      for (var i = 0; i < (int)reward; i++)
-      {
-        rewards.Add(reward);
-      }
-    }
+	foreach (CapturePointReward reward in Enum.GetValues(typeof(CapturePointReward)))
+	{
+	  for (var i = 0; i < (int)reward; i++)
+	  {
+		rewards.Add(reward);
+	  }
+	}
 
-    return rewards.Sample();
+	return rewards.Sample();
   }
 
   public override void _Process(double delta)
   {
-    if (_captured)
-    {
-      return;
-    }
+	if (_captured)
+	{
+	  return;
+	}
 
-    QueueRedraw();
-    var diffPlayers = CalcActiveCaptureDiff();
-    if (diffPlayers == 0)
-    {
-      ChargePlayerPlaying = false;
-      return;
-    }
+	QueueRedraw();
+	var diffPlayers = CalcActiveCaptureDiff();
+	if (diffPlayers == 0)
+	{
+	  ChargePlayerPlaying = false;
+	  return;
+	}
 
-    ChargePlayerPlaying = true;
+	ChargePlayerPlaying = true;
 
-    var diff = diffPlayers > 0 ? 1 : -1;
-    _captureTime += diff * (float)delta;
+	var diff = diffPlayers > 0 ? 1 : -1;
+	_captureTime += diff * (float)delta;
 
-    if (_captureTime >= TimeToCapture)
-    {
-      _captured = true;
-      CapturedListeners?.Invoke(this, Team.Light);
-    }
-    else if (_captureTime <= -TimeToCapture)
-    {
-      _captured = true;
-      CapturedListeners?.Invoke(this, Team.Dark);
-    }
+	if (_captureTime >= TimeToCapture)
+	{
+	  _captured = true;
+	  CapturedListeners?.Invoke(this, Team.Light);
+	}
+	else if (_captureTime <= -TimeToCapture)
+	{
+	  _captured = true;
+	  CapturedListeners?.Invoke(this, Team.Dark);
+	}
 
-    UpdateProgress();
+	UpdateProgress();
   }
 
   public override void _Ready()
   {
-    this.GetNodes();
-    var area = this.GetNodeOrExplode<Area2D>("Area2D");
-    area.BodyEntered += OnBodyEntered;
-    area.BodyExited += OnBodyExited;
+	this.GetNodes();
+	var area = this.GetNodeOrExplode<Area2D>("Area2D");
+	area.BodyEntered += OnBodyEntered;
+	area.BodyExited += OnBodyExited;
 
-    if (area.GetNodeOrExplode<CollisionShape2D>("CollisionShape2D").Shape is not CircleShape2D circle)
-    {
-      throw new NodeNotFoundException("CollisionShape2D.Shape is not a CircleShape2D");
-    }
+	if (area.GetNodeOrExplode<CollisionShape2D>("CollisionShape2D").Shape is not CircleShape2D circle)
+	{
+	  throw new NodeNotFoundException("CollisionShape2D.Shape is not a CircleShape2D");
+	}
 
-    Reward = GetRandomReward();
-    _rewardLabel.Text = Reward.ToString();
+	Reward = GetRandomReward();
+	_rewardLabel.Text = Reward.ToString();
 
-    _radius = circle.Radius;
+	_radius = circle.Radius;
   }
 
   private int CalcActiveCaptureDiff()
   {
-    var lightPlayers = _playersInside.Count(p => p.Team == Team.Light);
-    var darkPlayers = _playersInside.Count(p => p.Team == Team.Dark);
-    return lightPlayers - darkPlayers;
+	var lightPlayers = _playersInside.Count(p => p.Team == Team.Light);
+	var darkPlayers = _playersInside.Count(p => p.Team == Team.Dark);
+	return lightPlayers - darkPlayers;
   }
 
   private void OnBodyEntered(Node2D body)
   {
-    if (body is Player player)
-    {
-      _playersInside.Add(player);
-    }
+	if (body is Player player)
+	{
+	  _playersInside.Add(player);
+	}
   }
 
   private void OnBodyExited(Node2D body)
   {
-    if (body is Player player)
-    {
-      _playersInside.Remove(player);
-    }
+	if (body is Player player)
+	{
+	  _playersInside.Remove(player);
+	}
   }
 
   private void UpdateProgress()
   {
-    var progress = (_captureTime + TimeToCapture) / (TimeToCapture * 2);
-    _progressBar.Value = progress;
+	var progress = (_captureTime + TimeToCapture) / (TimeToCapture * 2);
+	_textureProgressBar.Value = progress;
   }
 }
--- game/Scripts/MenuControllers/StartScreen.cs
+++ game/Scripts/MenuControllers/StartScreen.cs
@@ -21,90 +21,90 @@ public partial class StartScreen : Node2D
 
   public override void _Ready()
   {
-    this.GetNodes();
-    GetTree().Paused = false;
+	this.GetNodes();
+	GetTree().Paused = false;
 
-    const string ButtonsRoot = "CanvasLayer/VBoxContainer/";
-    var startButton = this.GetNodeOrExplode<Button>(ButtonsRoot + "StartButton");
-    var quitButton = this.GetNodeOrExplode<Button>(ButtonsRoot + "QuitButton");
-    _roundTimeLineEdit = this.GetNodeOrExplode<SpinBox>(ButtonsRoot + "RoundTimeSpinBox");
-    _roundsToWinLineEdit = this.GetNodeOrExplode<SpinBox>(ButtonsRoot + "RoundsToWinSpinBox");
+	const string ButtonsRoot = "CanvasLayer/VBoxContainer/";
+	var startButton = this.GetNodeOrExplode<Button>(ButtonsRoot + "StartButton");
+	var quitButton = this.GetNodeOrExplode<Button>(ButtonsRoot + "QuitButton");
+	_roundTimeLineEdit = this.GetNodeOrExplode<SpinBox>(ButtonsRoot + "RoundTimeSpinBox");
+	_roundsToWinLineEdit = this.GetNodeOrExplode<SpinBox>(ButtonsRoot + "RoundsToWinSpinBox");
 
-    startButton.Pressed += StartGame;
-    quitButton.Pressed += QuitGame;
+	startButton.Pressed += StartGame;
+	quitButton.Pressed += QuitGame;
 
-    startButton.GrabFocus();
+	startButton.GrabFocus();
   }
 
   private void QuitGame()
   {
-    GetTree().Quit();
+	GetTree().Quit();
   }
 
   public override void _UnhandledInput(InputEvent @event)
   {
-    if (@event is InputEventJoypadButton e && e.Pressed)
-    {
-      if (e.ButtonIndex == JoyButton.Start)
-      {
-        // join game
+	if (@event is InputEventJoypadButton e && e.Pressed)
+	{
+	  if (e.ButtonIndex == JoyButton.Start)
+	  {
+		// join game
 
-        AddPlayer(e.Device);
-      }
-    }
+		AddPlayer(e.Device);
+	  }
+	}
   }
 
   private void AddPlayer(int deviceId)
   {
-    if (deviceId < 0)
-    {
-      return;
-    }
+	if (deviceId < 0)
+	{
+	  return;
+	}
 
-    if (_avatars.ContainsKey(deviceId))
-    {
-      return;
-    }
+	if (_avatars.ContainsKey(deviceId))
+	{
+	  return;
+	}
 
-    var margin = 200; // TODO: better calculation
-    var avatar = _avatarScene.Instantiate<Avatar>();
-    _avatarsRoot.AddChild(avatar);
-    avatar.Team = Team.Light;
-    avatar.DeviceId = deviceId;
-    avatar.Position = new Vector2(_avatars.Count * margin, 0);
+	var margin = 200; // TODO: better calculation
+	var avatar = _avatarScene.Instantiate<Avatar>();
+	_avatarsRoot.AddChild(avatar);
+	avatar.Team = Team.Light;
+	avatar.DeviceId = deviceId;
+	avatar.Position = new Vector2(_avatars.Count * margin, 0);
 
-    _avatars.Add(deviceId, avatar);
+	_avatars.Add(deviceId, avatar);
   }
 
   private void StartGame()
   {
-    if (!ValidTeams())
-    {
-      return;
-    }
+	if (!ValidTeams())
+	{
+	  return;
+	}
 
-    var roundTime = _roundTimeLineEdit.Value;
-    var roundsToWin = _roundsToWinLineEdit.Value;
+	var roundTime = _roundTimeLineEdit.Value;
+	var roundsToWin = _roundsToWinLineEdit.Value;
 
-    GlobalGameState.RoundTime = (int)roundTime;
-    GlobalGameState.RoundsToWin = (int)roundsToWin;
+	GlobalGameState.RoundTime = (int)roundTime;
+	GlobalGameState.RoundsToWin = (int)roundsToWin;
 
-    foreach (var (deviceId, avatar) in _avatars)
-    {
-      var playerValues = new GlobalGameState.PlayerValues(avatar.Team, avatar.PlayerName);
-      GlobalGameState.Players.Add(deviceId, playerValues);
-    }
+	foreach (var (deviceId, avatar) in _avatars)
+	{
+	  var playerValues = new GlobalGameState.PlayerValues(avatar.Team, avatar.PlayerName);
+	  GlobalGameState.Players.Add(deviceId, playerValues);
+	}
 
-    GetTree().ChangeSceneToFile(SceneResourceWrapper.WorldPath);
+	GetTree().ChangeSceneToFile(SceneResourceWrapper.WorldPath);
   }
 
   private bool ValidTeams()
   {
-    if (_avatars.Any(p => p.Value.Team == Team.Neutral))
-    {
-      return false;
-    }
+	if (_avatars.Any(p => p.Value.Team == Team.Neutral))
+	{
+	  return false;
+	}
 
-    return _avatars.Any();
+	return _avatars.Any();
   }
 }
